f14747661d122ebfcc00ba43fb225ec33ad84aec
AlAminAshik <alaminashik890.bd@gmail.com>
2026-08-07 21:21:15 +0600

    gpio code added, conv ratio changed

--- Codes/MainCLAT/bat_calc.h
+++ Codes/MainCLAT/bat_calc.h
@@ -1,7 +1,7 @@
 #ifndef BAT_CAL_H
 #define BAT_CAL_H
 
-#define FILTER_SIZE 20   // number of samples to average (20 samples @ 100Hz = 200ms window)
+#define FILTER_SIZE 50   // number of samples to average (50 samples)
 
 float voltage_to_soc(float voltage);
 float filter_voltage(float new_reading);    //moving average
--- Codes/MainCLAT/mainCLAT.c
+++ Codes/MainCLAT/mainCLAT.c
@@ -13,6 +13,10 @@
 #define ADC_PIN 26        // GP26 = ADC0. Also available: GP27 (ADC1), GP28 (ADC2)
 #define ADC_CHANNEL 0     // 0 for GP26, 1 for GP27, 2 for GP28
 
+// GPIO control
+#define BUZ_PIN 14
+#define Relay_PIN 8
+#define GRID_IN_PIN 15
 
 int main() {
     stdio_init_all();
@@ -33,12 +37,21 @@ int main() {
     // Wait a moment for USB serial to connect (helps catch early prints)
     sleep_ms(2000);
     
+    // GPIO setup
+    gpio_init(BUZ_PIN);
+    gpio_set_dir(BUZ_PIN, GPIO_OUT);
+    gpio_init(Relay_PIN);
+    gpio_set_dir(Relay_PIN, GPIO_OUT);
+    gpio_init(GRID_IN_PIN);
+    gpio_set_dir(GRID_IN_PIN, GPIO_IN);
+    gpio_put(GRID_IN_PIN, 1);  // Enable internal pull-up resistor
+
     const float conversion_factor = 3.30f / (1 << 12);  // 12-bit ADC, 3.3V reference (1 << 12 is just a slightly more "hardware-flavored" way of writing 4096 (2^12)))
 
 
     while (true) {
         uint16_t raw = adc_read();              // raw value: 0–4095
-        float voltage = raw * conversion_factor * 20; // Convert to actual voltage at the battery terminals  
+        float voltage = raw * conversion_factor * 19.0f; // Convert to actual voltage at the battery terminals  
         float filtered_voltage = filter_voltage(voltage); // Apply moving average filter
         float soc = voltage_to_soc(filtered_voltage);     // Convert voltage to SOC percentage
 
@@ -48,7 +61,23 @@ int main() {
         lcd_print("Voltage: "); lcd_print_num(filtered_voltage); lcd_print(" V");
         lcd_set_cursor(0, 1);
         lcd_print("SOC: "); lcd_print_num(soc); lcd_print(" %");
-        sleep_ms(300);
+        
+        if(soc < 40.0f) {
+            gpio_put(BUZ_PIN, 1); // Turn on buzzer
+        } else {
+            gpio_put(BUZ_PIN, 0); // Turn off buzzer
+        }
+        if(soc < 50.0f) {
+            gpio_put(Relay_PIN, 1); // Turn on relay
+        } else {
+            gpio_put(Relay_PIN, 0); // Turn off relay
+        }
+        
+        
+        sleep_ms(100);
+
+
+
         // lcd_clear();
     }
 }
